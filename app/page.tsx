8dd622fdccf4218113ddcbaeb4ac6797cfb6bfa3
zombie9 <darryl.banks@zombie9.com>
2024-10-02 15:42:58 +0100

    update eve login button styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center w-full min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex items-center justify-center w-full min-h-screen font-[family-name:var(--font-geist-mono)]">
       <div className="flex flex-col gap-4 items-center">
         <h1>EVE ONLINE CHARACTER SHEET</h1>
         <Button
-          className="w-[270px] h-[45px] bg-[url(./media/eve-sso-login-black-large.png)]"
+          className="w-[270px] h-[45px] bg-[url(./media/eve-sso-login-black-large.png)] data-[hover]:brightness-125"
           onClick={handleClick}
         ></Button>
       </div>
